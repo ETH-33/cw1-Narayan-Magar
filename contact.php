29a34eaaa945ad506e4fc4e5c39677d831660614
Narayan-Magar <124230893+Narayan-Magar@users.noreply.github.com>
2023-07-24 18:36:57 +0545

    Add files via upload

--- contact.php
+++ contact.php
@@ -29,55 +29,51 @@ if(isset($_POST['send'])){
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
+    <style>
+        .c{
+            justify-content: center;
+            text-align: center;
+            color: purple;
+            font-size: 5rem;
+            font-weight: 5rem;
+            margin-bottom: -2.5rem;
+        }
+        /* span{
+        } */
+    </style>
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>contact</title>
-
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-<body>
-   
 <?php include 'header.php'; ?>
+<link rel="stylesheet" href="css/style.css">
+<link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+<body>
 
-<div class="heading">
-   <h3>contact us</h3>
-   <p> <a href="home.php">home</a> / contact </p>
+<div>
+    <section class="c">
+
+        <span>Contact Us</span>
+    </section>
+   <!-- <h3>contact us</h3> -->
+   <!-- <p> <a href="home.php">home</a> / contact </p> -->
 </div>
-
 <section class="contact">
 
-   <form action="" method="post">
-      <h3>say something!</h3>
-      <input type="text" name="name" required placeholder="enter your name" class="box">
-      <input type="email" name="email" required placeholder="enter your email" class="box">
-      <input type="number" name="number" required placeholder="enter your number" class="box">
-      <textarea name="message" class="box" placeholder="enter your message" id="" cols="30" rows="10"></textarea>
-      <input type="submit" value="send message" name="send" class="btn">
-   </form>
+<form action="" method="post">
+   <h3>say something!</h3>
+   <input type="text" name="name" required placeholder="enter your name" class="box">
+   <input type="email" name="email" required placeholder="enter your email" class="box">
+   <input type="number" name="number" required placeholder="enter your number" class="box">
+   <textarea name="message" class="box" placeholder="enter your message" id="" cols="30" rows="10"></textarea>
+   <input type="submit" value="send message" name="send" class="btn">
+</form>
 
 </section>
-
-
-
-
-
-
-
-
-<?php include 'footer.php'; ?>
-
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
-
 </body>
+<?php include 'footer.php'; ?>
 </html>
